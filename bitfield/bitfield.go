cdb6a72cf1eb5d9098dbc9a361d543d577a60758
georgebuleu <georgebu44@gmail.com>
2024-05-12 02:40:37 +0300

    added functions to check if a piece exists nad to set a piece in the bitfield

--- bitfield/bitfield.go
+++ bitfield/bitfield.go
@@ -2,9 +2,22 @@ package bitfield
 
 type Bitfield []byte
 
-//func (bf Bitfield) SetBits(bits int) {
-//	idx := bits / 8
-//	offset := bits % 8
-//
-//
-//}
+// pieceNo = 9 => byteIdx = 9 / 8 = 1 and  offset = 1
+// mask = 00000010 => mask = byte(1 << 1)
+// bf = 11111111.11111111
+func (bf Bitfield) HavePiece(pieceNo int) bool {
+	byteIdx := pieceNo / 8
+	bitOffset := pieceNo % 8
+	mask := byte(1 << bitOffset)
+	return (bf[byteIdx] & mask) != 0
+}
+
+// pieceNo = 9 => byteIdx = 9 / 8 = 1 and  offset = 1
+// mask = 00000010 => mask = byte(1 << 1)
+// bf = 11111111.11111111
+func (bf Bitfield) SetPiece(pieceNo int) {
+	byteIdx := pieceNo / 8
+	bitOffset := pieceNo % 8
+	mask := byte(1 << bitOffset)
+	bf[byteIdx] = bf[byteIdx] | mask
+}
