590e5ee747807ccc87ad76854b4f69a63ab812dc
georgebuleu <georgebu44@gmail.com>
2024-05-19 14:59:52 +0300

    refactored and added functionality to send messages

--- peerconn/peerconn.go
+++ peerconn/peerconn.go
@@ -7,10 +7,8 @@ import (
 	"swiftpeer/client/bitfield"
 	"swiftpeer/client/handshake"
 	"swiftpeer/client/message"
-	"swiftpeer/client/torrent"
 	"swiftpeer/client/tracker"
 	"swiftpeer/client/utils"
-	"sync"
 	"time"
 )
 
@@ -22,47 +20,11 @@ type PeerConn struct {
 	pieces   bitfield.Bitfield
 }
 
-func (pc *PeerConn) initHandshake() error {
-	hs := handshake.NewHandshake(utils.GetPeerIdAsBytes(pc.peer.PeerId), pc.infoHash)
-	_, err := pc.conn.Write(hs.Serialize())
-	if err != nil {
-		return fmt.Errorf("failed to send handshake with %v : %v", pc.conn.RemoteAddr(), err)
-	}
-	answer, err := hs.Deserialize(io.Reader(pc.conn))
-	if err != nil {
-		if err != io.EOF {
-			return fmt.Errorf("Failed to READ: %v\n", err.Error())
-		} else {
-			return fmt.Errorf("peer closed the connection")
-		}
-	}
-	if hs.InfoHash != answer.InfoHash {
-
-		return fmt.Errorf("different info_hash during handshake")
-	}
-
-	return nil
-}
-
-func HandlePeers(peers []tracker.Peer, wg *sync.WaitGroup) {
-	for _, peer := range peers {
-		wg.Add(1)
-		go func(p tracker.Peer) {
-			_, err := NewPeerConn(p)
-			if err != nil {
-				fmt.Println(err)
-			}
-			wg.Done()
-		}(peer)
-	}
-}
-
-func NewPeerConn(peer tracker.Peer) (*PeerConn, error) {
+func NewPeerConn(peer tracker.Peer, infoHash [20]byte) (*PeerConn, error) {
 	address, err := peer.FormatAddress()
 	if err != nil {
 		return nil, err
 	}
-	hash, _ := torrent.HashInfo()
 	conn, err := net.DialTimeout("tcp", address, time.Second*20)
 
 	if err != nil {
@@ -73,21 +35,92 @@ func NewPeerConn(peer tracker.Peer) (*PeerConn, error) {
 	pc := &PeerConn{
 		conn:     conn,
 		peer:     peer,
-		infoHash: hash,
+		infoHash: infoHash,
 		isChoked: true,
 		pieces:   bitfield.Bitfield{},
 	}
 
-	err = pc.initHandshake()
+	err = pc.doHandshake()
 	if err != nil {
 		conn.Close()
 		return nil, err
 	}
-	msg := message.Read(io.Reader(conn))
-	fmt.Printf("First Message: %v: %v\n", msg.GetMessageName(), msg.Payload)
-	if msg.Id == message.BitfieldMsg {
-		pc.pieces = msg.Payload
+
+	err = pc.receiveBitfield()
+	if err != nil {
+		conn.Close()
+		return nil, err
 	}
 
 	return pc, nil
+}
+
+func (pc *PeerConn) doHandshake() error {
+	hs := handshake.NewHandshake(utils.GetPeerIdAsBytes(pc.peer.PeerId), pc.infoHash)
+	_, err := pc.conn.Write(hs.Serialize())
+	if err != nil {
+		return fmt.Errorf("failed to send handshake with %v : %v", pc.conn.RemoteAddr(), err)
+	}
+	response, err := hs.Deserialize(io.Reader(pc.conn))
+	if err != nil {
+		if err != io.EOF {
+			return fmt.Errorf("Failed to READ: %v\n", err.Error())
+		} else {
+			return fmt.Errorf("peer closed the connection")
+		}
+	}
+	if hs.InfoHash != response.InfoHash {
+
+		return fmt.Errorf("different info_hash during handshake")
+	}
+	fmt.Printf("Successfuly connected to: %v\n", pc.conn.LocalAddr())
+	return nil
+}
+
+func (pc *PeerConn) receiveBitfield() error {
+	pc.conn.SetDeadline(time.Now().Add(8 * time.Second))
+	defer pc.conn.SetDeadline(time.Time{})
+
+	msg, err := message.Read(pc.conn)
+	if err != nil {
+		return err
+	}
+
+	if msg.Id != message.BitfieldMsg {
+		return fmt.Errorf("expected bitdfield msg but got: %v", msg.Name())
+	}
+
+	pc.pieces = msg.Payload
+	return nil
+}
+
+func (pc *PeerConn) SendRequestMsg(pieceIndex, offset, length int) error {
+
+	m, err := message.NewRequest(pieceIndex, offset, length)
+	if err != nil {
+		return err
+	}
+	_, err = pc.conn.Write(m.Serialize())
+	return err
+}
+
+func (pc *PeerConn) SendInterested() error {
+	_, err := pc.conn.Write(message.NewInterested().Serialize())
+	return err
+}
+
+func (pc *PeerConn) SendNotInterested() error {
+	_, err := pc.conn.Write(message.NewNotInterested().Serialize())
+	return err
+}
+
+func (pc *PeerConn) SendUnchoke() error {
+	_, err := pc.conn.Write(message.NewUnchoke().Serialize())
+	return err
+}
+
+//TODO have message
+
+func (pc *PeerConn) Read() (*message.Message, error) {
+	return message.Read(pc.conn)
 }
