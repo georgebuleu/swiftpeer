f5cbadae5f9725df20821180233a19d04d338144
georgebuleu <georgebu44@gmail.com>
2024-08-21 08:53:17 +0300

    torrent.go is a new torrent instance now

--- torrent/torrent.go
+++ torrent/torrent.go
@@ -5,120 +5,98 @@ import (
 	"crypto/sha1"
 	"fmt"
 	"path/filepath"
-	"swiftpeer/client/bencode"
+	"swiftpeer/client/bitfield"
+	"swiftpeer/client/peer"
 	"swiftpeer/client/torrent/metadata"
 )
 
-const HashLen = sha1.Size
-
+// Torrent represents an active torrent download
 type Torrent struct {
-	Announce     string
-	AnnounceList [][]string
-	Name         string
+	Metadata     *metadata.Metadata
 	PieceLength  int
-	InfoHash     [HashLen]byte
-	PieceHashes  [][HashLen]byte
-	TotalLength  int
-	Files        []struct {
-		Length int
-		Path   string
-	}
-}
-type Info struct {
-	Pieces      string `bencode:"pieces"`
-	PieceLength int    `bencode:"piece length"`
-	Name        string `bencode:"name"`
-	Length      int    `bencode:"length"`
-	Files       []struct {
-		Length int      `bencode:"length"`
-		Path   []string `bencode:"path"`
-	} `bencode:"files"`
+	TotalLength  int64
+	NumPieces    int
+	Peers        peer.AddrSet
+	PiecesStatus bitfield.Bitfield // Bitfield of downloaded pieces
 }
 
-func NewTorrent() *Torrent {
-	m := metadata.NewMetadata()
-
-	if m == nil {
-		fmt.Printf("torrent: Failed to load metadata\n")
-		return nil
-	}
-
-	infoHash, err := hashInfo(m)
+func NewTorrent(path string) (*Torrent, error) {
+	md, err := metadata.NewMetadataFromFile(path)
 	if err != nil {
-		fmt.Printf("torrent: %v", err)
-		return nil
-	}
-	pieceHashes, err := splitPieces(m)
-	if err != nil {
-		fmt.Printf("torrent: %v", err)
-		return nil
-	}
-	if len(m.Info.Files) == 0 && m.Info.Length == 0 {
-		fmt.Printf("torrent: no length or files\n")
+		return nil, fmt.Errorf("failed to load metadata: %v", err)
 	}
 
 	t := &Torrent{
-		Announce:     m.Announce,
-		AnnounceList: m.AnnounceList,
-		Name:         m.Info.Name,
-		PieceLength:  m.Info.PieceLength,
-		InfoHash:     infoHash,
-		PieceHashes:  pieceHashes,
+		Metadata:    md,
+		PieceLength: md.Info.PieceLength,
+		TotalLength: md.TotalLength(),
+		Peers:       make(peer.AddrSet),
 	}
 
-	if m.Info.Length != 0 {
-		t.Files = append(t.Files, struct {
-			Length int
-			Path   string
-		}{
-			Length: m.Info.Length,
-			Path:   m.Info.Name,
-		})
-		t.TotalLength = m.Info.Length
-	} else {
-		for _, file := range m.Info.Files {
-			paths := append([]string{m.Info.Name}, file.Path...)
-			t.Files = append(t.Files, struct {
-				Length int
-				Path   string
-			}{
-				Length: file.Length,
-				Path:   filepath.Join(paths...),
-			})
-			t.TotalLength += file.Length
+	t.NumPieces = len(md.Info.Pieces) / sha1.Size
+	t.PiecesStatus = make([]byte, (t.NumPieces+7)/8)
+
+	return t, nil
+}
+
+func (t *Torrent) AddPeer(p peer.Peer) {
+	addr, err := p.FormatAddress()
+	if err == nil {
+		t.Peers[addr] = struct{}{}
+	}
+}
+
+func (t *Torrent) MarkPieceComplete(index int) {
+	byteIndex := index / 8
+	bitOffset := index % 8
+	t.PiecesStatus[byteIndex] |= 1 << uint(7-bitOffset)
+}
+
+func (t *Torrent) IsPieceComplete(index int) bool {
+	byteIndex := index / 8
+	bitOffset := index % 8
+	return t.PiecesStatus[byteIndex]&(1<<uint(7-bitOffset)) != 0
+}
+
+func (t *Torrent) DownloadedPieces() int {
+
+	return t.PiecesStatus.Count()
+
+}
+
+func (t *Torrent) Progress() float64 {
+	return float64(t.DownloadedPieces()) / float64(t.NumPieces)
+}
+
+func (t *Torrent) IsComplete() bool {
+	return t.DownloadedPieces() == t.NumPieces
+}
+
+// PieceSize computes the piece based of position
+func (t *Torrent) PieceSize(index int) int {
+	if index == t.NumPieces-1 {
+		return int(t.TotalLength % int64(t.PieceLength))
+	}
+	return t.PieceLength
+}
+
+func (t *Torrent) VerifyPiece(index int, pieceData []byte) bool {
+	var metadataHash [20]byte
+	copy(metadataHash[:], t.Metadata.Info.Pieces[index*sha1.Size:(index+1)*sha1.Size])
+	return bytes.Equal(metadataHash[:], pieceData)
+}
+
+func (t *Torrent) FilePathForPiece(index int) (string, int64) {
+	offset := int64(index) * int64(t.PieceLength)
+	// Single file torrent
+	if t.Metadata.Info.Length > 0 {
+		return t.Metadata.Info.Name, offset
+	}
+	for _, file := range t.Metadata.Files() {
+		if offset < int64(file.Length) {
+			return filepath.Join(file.Path...), offset
 		}
-
+		offset -= int64(file.Length)
 	}
-
-	return t
-}
-
-// hashes the info dict
-func hashInfo(m *metadata.Metadata) ([HashLen]byte, error) {
-	var buf bytes.Buffer
-	err := bencode.NewEncoder(&buf).Encode(m.Info)
-	//fmt.Printf("\ninfo: %s\n", buf.String()[:500])
-	if err != nil {
-		return [HashLen]byte{}, err
-	}
-
-	//fmt.Printf("\nhash: %v", hex.EncodeToString(sum[:]))
-
-	return sha1.Sum(buf.Bytes()), nil
-}
-
-func splitPieces(m *metadata.Metadata) ([][HashLen]byte, error) {
-	pieces := []byte(m.Info.Pieces)
-
-	if len(pieces)%sha1.Size != 0 {
-		return nil, fmt.Errorf("invalid pieces length")
-	}
-	numPieces := len(pieces) / sha1.Size
-	hashes := make([][HashLen]byte, numPieces)
-	for i := 0; i < len(pieces); i += HashLen {
-		var hash [HashLen]byte
-		copy(hash[:], pieces[i:i+HashLen])
-		hashes[i/20] = hash
-	}
-	return hashes, nil
+	return "", 0 // Should never happen if the torrent is valid
 }
