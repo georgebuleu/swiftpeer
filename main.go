93ebf0199a0cd14802113387dfa8ab65ba2968e5
georgebuleu <georgebu44@gmail.com>
2024-08-28 14:58:28 +0300

    removed statemanager.go and added the logic to torrent.go

--- main.go
+++ main.go
@@ -2,83 +2,26 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
 	"swiftpeer/client/common"
-	"swiftpeer/client/peer"
-	"swiftpeer/client/statemanager"
 	"swiftpeer/client/torrent"
-	"swiftpeer/client/tracker"
-	"time"
 )
 
 const Port int = 6881
 
 func main() {
-	torrentFilePath := "testdata/debian-12.5.0-amd64-netinst.iso.torrent"
-	peers := make(peer.AddrSet)
-	outDir := "/home/george/test_licenta/"
-	tf, err := torrent.NewTorrent(torrentFilePath)
-	if err != nil {
-		fmt.Println(err)
-	}
+
 	peerId := common.GeneratePeerId()
 
-	err = tracker.GetTorrentData(tf.Metadata.Announce, tf.Metadata.AnnounceList, Port, tf.InfoHash, peerId, peers)
-	if err != nil {
-		fmt.Println(err)
-	}
-	for p, _ := range peers {
-		fmt.Println(p)
-	}
-
-	fmt.Println("Peers2")
-	peers2 := make(peer.AddrSet)
-	time.Sleep(35 * time.Second)
-	err = tracker.GetTorrentData(tf.Metadata.Announce, tf.Metadata.AnnounceList, Port, tf.InfoHash, peerId, peers2)
+	torrentFilePath := "testdata/nasa.torrent"
+	outDir := "/home/george/test_licenta/"
+	_, err := torrent.NewTorrent(torrentFilePath, peerId, Port, outDir)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	for p, _ := range peers2 {
-		peers[p] = struct{}{}
-	}
+	//err = tf.Download(outDir)
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
 
-	err = DownloadFile(tf, peers, peerId, outDir)
-
-	fmt.Println()
-
-}
-
-func DownloadFile(tf *torrent.Torrent, peers peer.AddrSet, peerId [20]byte, outDir string) error {
-
-	t := &statemanager.Torrent{
-		Peers:       peers,
-		PeerID:      peerId,
-		InfoHash:    tf.InfoHash,
-		PieceHashes: tf.PieceHashes,
-		PieceLength: tf.PieceLength,
-		Length:      int(tf.TotalLength),
-		Name:        tf.Name,
-		Files:       make([]statemanager.FileData, len(tf.Files)),
-	}
-	for i, _ := range tf.Files {
-		t.Files[i].Path = tf.Files[i].Path
-		t.Files[i].Length = tf.Files[i].Length
-	}
-
-	for _, file := range t.Files {
-		outPath := filepath.Join(outDir, file.Path)
-		baseDir := filepath.Dir(outPath)
-		if _, err := os.Stat(baseDir); os.IsNotExist(err) {
-			if err := os.MkdirAll(baseDir, os.ModePerm); err != nil {
-				return fmt.Errorf("making output directory: %w", err)
-			}
-		}
-	}
-
-	if err := t.Download(outDir); err != nil {
-		return err
-	}
-	return nil
 }
--- torrent/torrent.go
+++ torrent/torrent.go
@@ -1,30 +1,24 @@
 package torrent
 
 import (
+	"bytes"
 	"crypto/sha1"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
-	"swiftpeer/client/bitfield"
+	"runtime"
 	"swiftpeer/client/filewriter"
+	"swiftpeer/client/message"
 	"swiftpeer/client/peer"
+	"swiftpeer/client/peerconn"
 	"swiftpeer/client/torrent/metadata"
+	"swiftpeer/client/tracker"
+	"time"
 )
 
-// Torrent represents an active torrent download
-type Torrent struct {
-	Metadata     *metadata.Metadata
-	PieceLength  int
-	TotalLength  int64
-	NumPieces    int
-	Peers        peer.AddrSet
-	PiecesStatus bitfield.Bitfield // Bitfield of downloaded pieces
-	Name         string
-	InfoHash     [20]byte
-	PieceHashes  [][20]byte
-	PeerID       [20]byte
-	Files        []FileData
-}
+const maxRequest = 5
+const maxBlockSize = 2 << 13
 
 type FileData struct {
 	Length     int
@@ -35,73 +29,329 @@ type FileData struct {
 	Start      int
 }
 
-func NewTorrent(path string) (*Torrent, error) {
-	md, err := metadata.NewMetadataFromFile(path)
+// Torrent used to store the necessary information to download  the peers
+type Torrent struct {
+	Name        string
+	TotalLength int
+	InfoHash    [20]byte
+	PieceHashes [][20]byte
+	PieceLength int
+	PeerID      [20]byte
+	Peers       peer.AddrSet
+	Files       []FileData
+}
+
+// used to track the progress of a piece
+type pieceState struct {
+	peerConn   *peerconn.PeerConn
+	index      int
+	downloaded int
+	requested  int
+	left       int
+	data       []byte
+}
+
+type pieceTask struct {
+	index  int
+	hash   [20]byte
+	length int
+}
+
+type pieceCompleted struct {
+	index int
+	buf   []byte
+}
+
+func NewTorrent(pathToTorrentFile string, peerId [20]byte, port int, outDir string) (*Torrent, error) {
+	md, err := metadata.NewMetadataFromFile(pathToTorrentFile)
 	if err != nil {
 		return nil, fmt.Errorf("failed to load metadata: %v", err)
 	}
+	pHashes, err := md.PieceHashes()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get piece hashes: %v", err)
+	}
+
+	peers := make(peer.AddrSet)
+
+	err = tracker.GetTorrentData(md.Announce, md.AnnounceList, port, md.InfoHash, peerId, peers)
+	if err != nil {
+		return nil, err
+	}
+	//TODO:remove this print
+	for p, _ := range peers {
+		fmt.Println(p)
+	}
 
 	t := &Torrent{
-		Metadata:    md,
-		PieceLength: md.Info.PieceLength,
-		TotalLength: md.TotalLength(),
-		Peers:       make(peer.AddrSet),
-		Name:        md.Info.Name,
+		Peers:       peers,
+		PeerID:      peerId,
 		InfoHash:    md.InfoHash,
+		PieceHashes: pHashes,
+		PieceLength: md.Info.PieceLength,
+		Name:        md.Info.Name,
+		Files:       make([]FileData, 0, len(md.Info.Files)),
 	}
 
-	t.NumPieces = len(md.Info.Pieces) / sha1.Size
-	t.PiecesStatus = make([]byte, (t.NumPieces+7)/8)
+	if md.Info.Length != 0 {
+		t.Files = append(t.Files, FileData{
+			Length: md.Info.Length,
+			Path:   md.Info.Name,
+		})
+		t.TotalLength = md.Info.Length
+	} else {
+		for _, file := range md.Info.Files {
+			paths := append([]string{md.Info.Name}, file.Path...)
+			t.Files = append(t.Files, FileData{
+				Length: file.Length,
+				Path:   filepath.Join(paths...),
+			})
+			t.TotalLength += file.Length
+		}
 
-	// Initialize PieceHashes
-	t.PieceHashes = make([][20]byte, t.NumPieces)
-	for i := 0; i < t.NumPieces; i++ {
-		copy(t.PieceHashes[i][:], md.Info.Pieces[i*20:(i+1)*20])
 	}
 
-	// Initialize Files
-	t.Files = make([]FileData, len(md.Files()))
-	for i, file := range md.Files() {
-		t.Files[i] = FileData{
-			Length: file.Length,
-			Path:   filepath.Join(file.Path...),
+	for _, file := range t.Files {
+		outPath := filepath.Join(outDir, file.Path)
+		baseDir := filepath.Dir(outPath)
+		if _, err := os.Stat(baseDir); os.IsNotExist(err) {
+			if err := os.MkdirAll(baseDir, os.ModePerm); err != nil {
+				return nil, fmt.Errorf("making output directory: %w", err)
+			}
 		}
 	}
 
 	return t, nil
 }
 
-// Existing methods...
+func (s *pieceState) handleMessage() error {
 
-func (t *Torrent) ComputeBounds(index int) (int, int) {
+	m, err := s.peerConn.Read()
+	if err != nil {
+		return err
+	}
+
+	//checks for keep alive message which has length 0, no id and no payload
+	if m == nil {
+		return nil
+	}
+
+	switch m.Id {
+	case message.ChokeMsg:
+		s.peerConn.IsChoked = true
+	case message.UnchokeMsg:
+		s.peerConn.IsChoked = false
+	case message.PieceMsg:
+		received, err := m.ProcessPieceMsg(s.index, s.data)
+		if err != nil {
+			fmt.Println("error during piece message")
+			return err
+		}
+		s.downloaded += received
+		s.left--
+	case message.HaveMsg:
+		index, err := m.ProcessHaveMsg()
+		if err != nil {
+			fmt.Println("error during unchoke message")
+			return err
+		}
+		s.peerConn.Pieces.SetPiece(index)
+	default:
+		return nil
+	}
+	return nil
+}
+
+func prepareDownload(pc *peerconn.PeerConn, task *pieceTask) ([]byte, error) {
+	state := pieceState{
+		peerConn: pc,
+		index:    task.index,
+		data:     make([]byte, task.length),
+	}
+
+	pc.Conn.SetDeadline(time.Now().Add(25 * time.Second))
+	defer pc.Conn.SetDeadline(time.Time{})
+
+	for state.downloaded < task.length {
+		if !state.peerConn.IsChoked {
+			for state.left < maxRequest && state.requested < task.length {
+				blockSize := maxBlockSize
+
+				if task.length-state.requested < blockSize {
+					blockSize = task.length - state.requested
+				}
+
+				err := pc.SendRequestMsg(task.index, state.requested, blockSize)
+				if err != nil {
+					log.Printf("Failed to send request message for piece %d: %v\n", task.index, err)
+					return nil, err
+				}
+				state.left++
+				state.requested += blockSize
+			}
+		}
+
+		err := state.handleMessage()
+		if err != nil {
+			fmt.Println("\nerror while handling message in preparing download")
+			return nil, err
+		}
+	}
+
+	return state.data, nil
+}
+
+func (t *Torrent) startTask(peer string, pieceQueue chan *pieceTask, completed chan *pieceCompleted) {
+	pc, err := peerconn.NewPeerConn(peer, t.InfoHash)
+
+	if err != nil {
+		fmt.Printf("Failed to complete the handshake with %v. Disconnecting\n", peer)
+		return
+	}
+
+	defer pc.Conn.Close()
+
+	fmt.Printf("Completed the handshake with %v.\n", peer)
+
+	err = pc.SendUnchoke()
+	if err != nil {
+		fmt.Printf("Failed to send unchoke to %v: %v\n", peer, err)
+	}
+
+	err = pc.SendInterested()
+	if err != nil {
+		fmt.Printf("Failed to send interested to %v: %v\n", peer, err)
+	}
+
+	for pieceTask := range pieceQueue {
+		if !pc.Pieces.HasPiece(pieceTask.index) {
+			pieceQueue <- pieceTask
+			continue
+		}
+
+		buff, err := prepareDownload(pc, pieceTask)
+		if err != nil {
+			fmt.Printf("\nError downloading piece %d from %v:\n", pieceTask.index, peer)
+			fmt.Println(err)
+			pieceQueue <- pieceTask
+			return
+		}
+
+		valid := checkIntegrity(pieceTask, buff)
+		if !valid {
+			pieceQueue <- pieceTask
+			continue
+		} else {
+			pc.SendHave(pieceTask.index)
+			completed <- &pieceCompleted{pieceTask.index, buff}
+		}
+	}
+}
+
+func (t *Torrent) computeBounds(index int) (int, int) {
 	begin := index * t.PieceLength
 	end := begin + t.PieceLength
 
-	if end > int(t.TotalLength) {
-		end = int(t.TotalLength)
+	if end > t.TotalLength {
+		end = t.TotalLength
 	}
 	return begin, end
 }
 
-func (t *Torrent) ComputeSize(index int) int {
-	begin, end := t.ComputeBounds(index)
+func (t *Torrent) computeSize(index int) int {
+	begin, end := t.computeBounds(index)
 	return end - begin
 }
 
-func (t *Torrent) ComputeBoundsForFile(file FileData) (int, int) {
+func checkIntegrity(task *pieceTask, data []byte) bool {
+	h := sha1.Sum(data)
+	if !bytes.Equal(h[:], task.hash[:]) {
+		fmt.Printf("index %v did not pass integrity check\n", task.index)
+		return false
+	}
+	return true
+}
+
+func (t *Torrent) Download(path string) error {
+
+	if err := t.setupFiles(path); err != nil {
+		return err
+	}
+	defer t.finalCleanup()
+
+	fmt.Printf("Starting download for%v\n", t.Name)
+	piecesQueue := make(chan *pieceTask, len(t.PieceHashes))
+	completed := make(chan *pieceCompleted)
+	for idx, hash := range t.PieceHashes {
+		length := t.computeSize(idx)
+		piecesQueue <- &pieceTask{idx, hash, length}
+	}
+
+	for p, _ := range t.Peers {
+		go t.startTask(p, piecesQueue, completed)
+
+	}
+	log.Printf("pieces in compeleted %v out of %v\n", len(completed), len(t.PieceHashes))
+
+	finishedPieces := 0
+
+	timeout := time.After(20 * time.Second)
+
+	for finishedPieces < len(t.PieceHashes) {
+		select {
+		case piece := <-completed:
+			// Directly write to the appropriate file using memory-mapped region
+			if err := t.handlePiece(piece.index, piece.buf); err != nil {
+				fmt.Printf("Failed to handle piece %d: %v\n", piece.index, err)
+				return err
+			}
+			finishedPieces++
+			log.Printf("Downloaded piece #%d out of #%d\n", finishedPieces, len(t.PieceHashes))
+			timeout = time.After(30 * time.Second) // Reset timeout after each successful piece handling
+			log.Printf("(%0.2f%%) Downloaded piece #%d from %d peers\n", float64(finishedPieces)/float64(len(t.PieceHashes))*100, piece.index, runtime.NumGoroutine()-1)
+
+		case <-timeout:
+			fmt.Printf("Timeout: No pieces completed within the last 30 seconds")
+			return fmt.Errorf("download timeout")
+		}
+	}
+	close(piecesQueue)
+
+	return nil
+}
+
+func (t *Torrent) setupFiles(basePath string) error {
+	currentPosition := 0
+	for i, file := range t.Files {
+		fullPath := filepath.Join(basePath, file.Path)
+		if err := os.MkdirAll(filepath.Dir(fullPath), os.ModePerm); err != nil {
+			return err
+		}
+		writer, err := filewriter.New(fullPath, file.Length)
+		if err != nil {
+			return err
+		}
+		t.Files[i].Writer = writer
+		t.Files[i].Start = currentPosition
+		currentPosition += file.Length
+	}
+	return nil
+}
+
+func (t *Torrent) computeBoundsForFile(file FileData) (int, int) {
 	return file.Start, file.Start + file.Length
 }
 
-func (t *Torrent) HandlePiece(pieceIndex int, pieceData []byte) error {
-	begin, end := t.ComputeBounds(pieceIndex)
+func (t *Torrent) handlePiece(pieceIndex int, pieceData []byte) error {
+	begin, end := t.computeBounds(pieceIndex)
 
-	for i := range t.Files {
-		file := &t.Files[i]
+	for _, file := range t.Files {
 		if file.Completed {
 			continue
 		}
 
-		fileStart, fileEnd := t.ComputeBoundsForFile(*file)
+		fileStart, fileEnd := t.computeBoundsForFile(file)
+		//Check if the piece is between the bounds of the file
 		if begin < fileEnd && end > fileStart {
 			overlapStart := max(begin, fileStart)
 			overlapEnd := min(end, fileEnd)
@@ -111,6 +361,7 @@ func (t *Torrent) HandlePiece(pieceIndex int, pieceData []byte) error {
 				return err
 			}
 
+			// Update downloaded count and check for completion
 			file.Downloaded += len(data)
 			if file.Downloaded >= file.Length {
 				file.Completed = true
@@ -126,47 +377,14 @@ func (t *Torrent) HandlePiece(pieceIndex int, pieceData []byte) error {
 	return nil
 }
 
-func (t *Torrent) SetupFiles(basePath string) error {
-	currentPosition := 0
-	for i := range t.Files {
-		file := &t.Files[i]
-		fullPath := filepath.Join(basePath, file.Path)
-		if err := os.MkdirAll(filepath.Dir(fullPath), os.ModePerm); err != nil {
-			return err
-		}
-		writer, err := filewriter.New(fullPath, file.Length)
-		if err != nil {
-			return err
-		}
-		file.Writer = writer
-		file.Start = currentPosition
-		currentPosition += file.Length
-	}
-	return nil
-}
-
-func (t *Torrent) FinalCleanup() error {
-	for i := range t.Files {
-		file := &t.Files[i]
-		if !file.Completed && file.Writer != nil {
-			file.Writer.Sync()
-			file.Writer.Close()
+func (t *Torrent) finalCleanup() error {
+	for _, file := range t.Files {
+		if !file.Completed {
+			if file.Writer != nil {
+				file.Writer.Sync()
+				file.Writer.Close()
+			}
 		}
 	}
 	return nil
-}
-
-// Helper functions
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
 }
