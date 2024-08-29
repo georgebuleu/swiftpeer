b019724fc97b0426b29c55eef13ad4af86b3cb2e
georgebuleu <georgebu44@gmail.com>
2024-08-29 21:50:18 +0300

    added argument parsing

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 	"swiftpeer/client/common"
 	"swiftpeer/client/torrent"
 )
@@ -10,19 +12,26 @@ const Port int = 6881
 
 func main() {
 
+	torrentFilePath := flag.String("t", "", "Path to the torrent file")
+	outDir := flag.String("o", "", "Output directory for downloaded files")
+	flag.Parse()
+
+	if *torrentFilePath == "" || *outDir == "" {
+		fmt.Println("Usage: program -t <torrent-file-path> -o <output-directory>")
+		os.Exit(1)
+	}
+
 	peerId := common.GeneratePeerId()
 
-	torrentFilePath := "testdata/debian-12.5.0-amd64-netinst.iso.torrent"
-	outDir := "/home/george/test_licenta/"
-	t, err := torrent.NewTorrent(torrentFilePath, peerId, Port, outDir)
+	t, err := torrent.NewTorrent(*torrentFilePath, peerId, Port, *outDir)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error creating torrent:", err)
 		return
 	}
 
-	err = t.Download(outDir)
+	err = t.Download(*outDir)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error downloading torrent:", err)
 	}
 
 }
